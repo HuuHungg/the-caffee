1378364e29e74d44faed8e6fe298603955af916d
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-06 11:24:32 +0700

    add all file thecafe

---
+++ index.js
@@ -0,0 +1,5 @@
+const btnMenu = document.querySelector('.js-menu-click')
+const btnDrink = document.querySelector('. js-menu-click')
+
+const contentDrink = document.querySelector('.menu--footer-content')
+
